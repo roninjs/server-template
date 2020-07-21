38d646bd281250f4f102a6f91b605aa364a0ed76
peter <peter.mckee@gmail.com>
2020-07-21 13:42:30 -0500

    add server.js and package.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const ronin 		= require( 'ronin-server' )
+const mocks 		= require( 'ronin-mocks' )
+
+async function main() {
+
+	try {
+
+    const server = ronin.server({
+			port: 8080
+		})
+
+		server.use( '/services/m/', mocks.server( server.Router()) )
+
+    const result = await server.start()
+    console.info( result )
+
+	} catch( error ) {
+		console.error( error )
+	}
+
+}
+
+main()
